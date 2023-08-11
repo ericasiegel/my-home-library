0a9db75d79df61c42c9348d886684d8b03725258
Erica Siegel <siegel.erica@gmail.com>
2023-08-11 11:59:23 -0700

    setup admin models and display relationships

--- server/catalog/admin.py
+++ server/catalog/admin.py
@@ -1,3 +1,43 @@
+
 from django.contrib import admin
 
+from .models import *
+
+# generic function to get a string of any many to many field
+def concatenate_related(obj, related_field_name):
+    related_objects = getattr(obj, related_field_name).all()
+    names = [str(related_obj) for related_obj in related_objects]
+    return ', '.join(names)
+
+
+
 # Register your models here.
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ['id', 'type']
+    
+@admin.register(Series)
+class SeriesAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'series', 'series_number', 'author_names', 'read', 'description', 'genre_list']
+    list_editable = ['read']
+    
+    def get_queryset(self, request):
+        return super().get_queryset(request).prefetch_related('authors', 'genres')
+    
+    def author_names(self, book):
+        return concatenate_related(book, 'authors')
+    author_names.short_description = 'Author(s)'
+    
+    def genre_list(self, book):
+        return concatenate_related(book, 'genres')
+    genre_list.short_description = 'Genres'
+    
--- server/catalog/models.py
+++ server/catalog/models.py
@@ -4,18 +4,28 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=255)
     
+    def __str__(self):
+        return self.name
+    
 class Genre(models.Model):
     type = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.type
     
 class Series(models.Model):
     name = models.CharField(max_length=255)
     
+    def __str__(self):
+        return self.name
+    
 class Book(models.Model):
     title = models.CharField(max_length=255)
-    series = models.ForeignKey(Series, null=True, on_delete=models.CASCADE, related_name='series_books')
-    author = models.ManyToManyField(Author, related_name='author_books')
+    series = models.ForeignKey(Series, blank=True, null=True, on_delete=models.CASCADE, related_name='series_books', )
+    series_number = models.IntegerField(blank=True, null=True)
+    authors = models.ManyToManyField(Author, related_name='author_books')
     read = models.BooleanField(default=False)
     description = models.CharField(max_length=500)
-    genre = models.ManyToManyField(Genre, related_name='genre_books')
+    genres = models.ManyToManyField(Genre, related_name='genre_books')
     # picture = models.ImageField()
     
