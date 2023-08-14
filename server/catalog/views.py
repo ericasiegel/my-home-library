dc7e7b8a8bf4fe94d7247c04cbfd2b38d49d0f62
Erica Siegel <siegel.erica@gmail.com>
2023-08-13 18:44:58 -0700

    added ordering to viewsets

--- server/catalog/views.py
+++ server/catalog/views.py
@@ -1,5 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import *
 from .serializers import *
@@ -10,17 +10,19 @@ class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     
-    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ['authors__id', 'genres__id', 'series__id', 'read']
     search_fields = ['title', 'series__name', 'authors__name', 'genres__type']
+    ordering_fields = ['title', 'authors__name', 'series__name']
 
 
 class AuthorViewSet(ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
     
-    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     search_fields = ['name', 'author_books__title', 'author_series__name']
+    ordering_fields = ['name', 'author_books__title', 'author_series__name']
     
 class SeriesViewSet(ModelViewSet):
     queryset = Series.objects.all()
@@ -29,9 +31,12 @@ class SeriesViewSet(ModelViewSet):
     filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_fields = ['author_id']
     search_fields = ['name', 'author__name']
+    ordering_fields = ['name', 'author__name']
 
     
 class GenreViewSet(ModelViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
 
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    ordering_fields = ['type']
